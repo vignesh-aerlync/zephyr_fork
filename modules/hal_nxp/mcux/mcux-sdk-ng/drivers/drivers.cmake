634aa033eeae80379402080ea94dbe7f0f83c680
Surya Prakash T <suryat@aerlync.com>
2026-05-12 19:34:21 +0530

    modules: hal_nxp: mcux: enable lpc_adc driver
    Enable the lpc_adc driver to LPC84x series, disable the SDK internal
    clock control to allow Zephyr to manage the ADC clocks.
    
    Signed-off-by: Surya Prakash T <suryat@aerlync.com>
    Signed-off-by: Sayooj K Karun <sayooj@aerlync.com>

--- modules/hal_nxp/mcux/mcux-sdk-ng/drivers/drivers.cmake
+++ modules/hal_nxp/mcux/mcux-sdk-ng/drivers/drivers.cmake
@@ -320,6 +320,8 @@ if(CONFIG_SOC_SERIES_LPC84X)
   set_variable_ifdef(CONFIG_UART_LPC84X          CONFIG_MCUX_COMPONENT_driver.lpc_miniusart)
   zephyr_library_compile_definitions_ifdef(CONFIG_UART_LPC84X FSL_SDK_DISABLE_DRIVER_CLOCK_CONTROL=1)
   set_variable_ifdef (CONFIG_SOC_FLASH_LPC84X_IAP CONFIG_MCUX_COMPONENT_driver.iap)
+  set_variable_ifdef(CONFIG_ADC_LPC84X 		CONFIG_MCUX_COMPONENT_driver.lpc_adc)
+  zephyr_library_compile_definitions_ifdef(CONFIG_ADC_LPC84X FSL_SDK_DISABLE_DRIVER_CLOCK_CONTROL=1)
 endif()
 
 if(CONFIG_SOC_LPC55S36)
